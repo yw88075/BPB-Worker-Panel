c14b4a8c275bee40714cbd98f57dbd880d393b63
yw88075 <126597268+yw88075@users.noreply.github.com>
2024-07-20 16:14:28 +0800

    Add files via upload

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'f917bae7-c13c-4c0c-8f98-3b4b82e6ff03';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
